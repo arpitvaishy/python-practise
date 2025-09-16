5e2d72211284b9c125fa1e1dcdca5b1c5657be97
Arpit Vaishy <org.arpitvaishy@gmail.com>
2025-09-16 09:25:29 +0530

    Add dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,4 @@
+#print a dict containing 7 wonders of the world with the cities and countries they are in.
+wonders = {"petra": "jordan", "taj mahal": "agra", "great wall of china": "china", "christ the redemeer": "rio de janeiro", "machu pichu": "peru", "colosseum": "rome", "chichen itza": "mexico", }
+print(wonders.values())
+print(wonders.keys())
