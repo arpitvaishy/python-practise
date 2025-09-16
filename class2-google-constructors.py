4511239f9a814c5e7b25c1263b0a8026355bd0eb
Arpit Vaishy <org.arpitvaishy@gmail.com>
2025-09-16 09:13:28 +0530

    Add class2-google-constructors.py

---
+++ class2-google-constructors.py
@@ -0,0 +1,13 @@
+class Google:
+    def __init__(self, position, name, salary, net_worth):
+        self.position = position
+        self.name = name
+        self.salary = salary
+        self.net_worth = net_worth
+    def get_salary(self):
+        return self.salary
+    def get_info(self):
+        print(f"The {self.position} of Google is {self.name}, his yearly salary is {self.salary}, his net worth is about {self.net_worth}.")
+    
+a = Google("CEO", "Sundar Pichai", "10,000,000 $", "1.3 Billion $")
+a.get_info()
