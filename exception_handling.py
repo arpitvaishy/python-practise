7a23b8c598620f3abaeec5026ea0c99df2841546
Arpit Vaishy <org.arpitvaishy@gmail.com>
2025-09-19 18:10:54 +0530

    Add exception_handling.py

---
+++ exception_handling.py
@@ -0,0 +1,7 @@
+while True:
+    try:
+        a = input("Who is your favorite actress :")
+        b = input("Oh my my! \n Okay, now who is your favorite Actor then :")
+
+    except Exception as s:
+        print("Please type a name.", s)
