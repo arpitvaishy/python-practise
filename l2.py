f30e3ff69e87788d07e6df131e5c9ba0ae320e79
Arpit Vaishy <org.arpitvaishy@gmail.com>
2025-09-26 19:08:04 +0530

    Add l2.py

---
+++ l2.py
@@ -0,0 +1,16 @@
+n = int(input())
+for i in range(1, n + 1):
+    print(i, end=" ")
+
+    '''Key Points:
+input():
+
+Reads input from the user as a string.
+int(input()) converts the input to an integer.
+range(1, n + 1):
+
+Generates numbers starting from 1 up to n (inclusive).
+print(i, end=""):
+
+Prints the current number i without moving to a new line.
+The end="" argument ensures that the numbers are printed on the same line with spaces.'''
