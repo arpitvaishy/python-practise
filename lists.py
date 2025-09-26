7dd23ee3128cbbb30ad6e8027e87dd4ad23a540f
Arpit Vaishy <org.arpitvaishy@gmail.com>
2025-09-26 19:03:50 +0530

    lists.py

---
+++ lists.py
@@ -0,0 +1,14 @@
+colour = ["Red", "Green", "Blue", "Grey", "Brown", "Yellow"]
+print(colour)
+colour.append(5)
+print(colour)
+colour.insert(3, 10)
+print(colour)
+colour.reverse()
+print(colour)
+
+table = [8.2*i for i in range (1,11)]
+print(table)
+print(table.count(82))
+table.pop()
+print(table)
